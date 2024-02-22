25d8653be2330923ab9d21622352cd414e61ef82
huseyinkedik <159181717+huseyinkedik@users.noreply.github.com>
2024-02-22 10:42:02 +0300

    v2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        int a = 0;
+        System.out.print("Basamak sayısını giriniz :");
+        a = input.nextInt();
+        for (int i = 0; i < a; i++) {
+            for (int b = 1; b <= (a - i); b++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
     }
 }
